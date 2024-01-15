061c3fecd2214e4526e435bbbb7a79fae5dcf058
Semanur Yildirim <yildirim.semanur@icloud.com>
2024-01-15 22:52:50 +0100

    Initial Commit

---
+++ PomodoroTimer/PomodoroTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroTimerApp.swift
+//  PomodoroTimer
+//
+//  Created by Semanur Yildirim on 15.01.24.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
