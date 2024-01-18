f43d7f4c8c8c769d69138d586855a524f154f21c
Semanur Yildirim <yildirim.semanur@icloud.com>
2024-01-18 17:56:49 +0100

    updated views of timer description (focus/short break etc.) and session counter

--- PomodoroTimer/Views/TimerView.swift
+++ PomodoroTimer/Views/TimerView.swift
@@ -15,29 +15,16 @@ struct TimerView : View {
     var body: some View {
         NavigationStack{
             VStack(spacing: 30){
-                Text(message)
-                    .font(.system(size: 20)).bold()
-                Text("Session \(timerManager.getSessions())")
-                progressRing
+                stateMessage
+                ZStack {
+                    progressRing
+                    sessionView
+                        .offset(y: 50)
+                }
                 buttons
             }
         }
     }
-    
-    var message : String {
-        switch timerManager.getMode() {
-        case .working:
-            return (timerManager.running() ? "Focus on your work. 🗂️" : "Start a session! 🚀" )
-        case .resting:
-            switch timerManager.inShortBreak() {
-            case true:
-                return "Grab a quick coffee!☕️"
-            default:
-                return "Time for a long refreshing timeout. 🏖️"
-            }
-        }
-    }
-    
     private func startTimer() {
         timer?.invalidate() // invalidate former timer if existent
         timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
@@ -48,6 +35,36 @@ struct TimerView : View {
     }
     
     // all below: components used for UI
+    
+    private var stateMessage : some View {
+        Text(message)
+            .font(.system(size: 15))
+            .bold()
+            .monospaced()
+            .foregroundStyle(Color.gray)
+    }
+    
+    private var message : String {
+        switch timerManager.getMode() {
+        case .working:
+            return (timerManager.running() ? "FOCUS" : "START A SESSION" )
+        case .resting:
+            switch timerManager.inShortBreak() {
+            case true:
+                return "SHORT BREAK"
+            default:
+                return "LONG BREAK"
+            }
+        }
+    }
+    
+    private var sessionView : some View {
+        Text("SESSION \(timerManager.getSessions())")
+            .font(.system(size: 13))
+            .monospaced()
+            .foregroundStyle(Color.gray)
+    }
+    
     
     private var progressRing : some View {
         ZStack(alignment: .center) {
@@ -84,7 +101,7 @@ struct TimerView : View {
     private var progressBar : some View {
         Circle()
             .trim(from:0, to: timerManager.getProgress())
-            .stroke(Color.pink.opacity(0.5), style: StrokeStyle(lineWidth: 10, lineCap: .round)) 
+            .stroke(Color.pink.opacity(0.5), style: StrokeStyle(lineWidth: 10, lineCap: .round))
             .frame(width: 230)
             .rotationEffect(Angle(degrees: 270))
             .animation(.easeOut, value: timerManager.getProgress())
@@ -92,6 +109,7 @@ struct TimerView : View {
     
     
     // buttons: setState/resetSessions
+    
     /// dynamic button to set the timer's state: starts if it's not running, else resets.
     private var setStateButton : some View  {
         switch timerManager.running() { // check state of timer: running (true/false)
@@ -120,8 +138,8 @@ struct TimerView : View {
     
 }
 
-/*
- #Preview {
- TimerView()
- }
- */
+
+#Preview {
+    TimerView()
+}
+
